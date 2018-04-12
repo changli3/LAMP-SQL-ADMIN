bc28ed37d9a6445d5bd362fcee2801c119a14e17
Chang Li <chang.li3@treasury.gov>
2018-04-12 17:56:39 -0400

    init

---
+++ config/lang.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is a part of MyWebSQL package
+ * defines languages supported by the application
+ *
+ * @file:      config/lang.php
+ * @author     Samnan ur Rehman
+ * @copyright  (c) 2008-2014 Samnan ur Rehman
+ * @web        http://mywebsql.net
+ * @license    http://mywebsql.net/license
+ */
+
+	$_LANGUAGES = array (
+		'en' => 'English'
+	);
+
+?>
---
+++ config/servers.php
@@ -0,0 +1,6 @@
+<?php
+	// existing one from the list
+	$ALLOW_CUSTOM_SERVERS = TRUE;
+	$ALLOW_CUSTOM_SERVER_TYPES = "mysql,pgsql";
+
+?>
